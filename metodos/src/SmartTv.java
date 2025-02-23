3bee7dd50619d43a407d1093c61ebe14fa4ab6bd
Bruno <bruno12sobrinho@gmail.com>
2025-02-23 17:01:24 -0300

    Adicionando aprendizado de escopo

--- metodos/src/SmartTv.java
+++ metodos/src/SmartTv.java
@@ -1,13 +1,14 @@
 public class SmartTv {
 
+    //As variaveis abaixo podem ser utilizadas em qualquer parte da classe;
     boolean ligada = false;
     int canal = 1;
     int volume = 20;
 
-    //Metodos são ações, nesta classe estamos criando vários metodos
+    //Metodos são ações, nesta classe estamos criando vários metodos;
 
     public void mudarCanal(int novoCanal){
-        canal=novoCanal;
+        canal=novoCanal; //A variavel novoCanal só pode ser acessada no metodo mudarCanal, não sendo possível acessa-la em outro escopo do código;
     }
 
     public void aumentarVolume(){
